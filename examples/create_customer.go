c823356240200d6dcf1e286a5e1888da7c1d5294
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-03 15:30:46 -0600

    added example creating customer

---
+++ examples/create_customer.go
@@ -0,0 +1,38 @@
+// Create a Customer with a default Card and some Metadata. To run this on your
+// system.
+//
+// STRIPE_SECRET_KEY=sk_your_key go run examples/create_customer.go
+package main
+
+import (
+  "fmt"
+  "os"
+  "github.com/stripe/stripe-go/stripe"
+)
+
+func main() {
+  client := stripe.NewClient(os.Getenv("STRIPE_SECRET_KEY"))
+
+  params := stripe.CustomerParams{
+    Description: "A pretty awesome customer",
+    Email: "apt@stripe.com",
+    CardParams: &stripe.CardParams{
+      Name: "Andrew Thorp",
+      Number: "4242424242424242",
+      ExpMonth: 01,
+      ExpYear: 2020,
+    },
+    Metadata: map[string]string{
+      "awesome": "yes",
+      "twitter": "@andrewpthorp",
+    },
+  }
+
+  customer, err := client.Customers.Create(&params)
+
+  if err != nil {
+    fmt.Println("Error: ", err)
+  } else {
+    fmt.Printf("%+v\n", customer)
+  }
+}
