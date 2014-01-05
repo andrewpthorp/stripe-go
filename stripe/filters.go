9308c12265a056af83186045259728e063a50ffb
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-05 15:17:03 -0800

    clarification docs of filters

--- stripe/filters.go
+++ stripe/filters.go
@@ -7,6 +7,14 @@ type Filters map[string]string
 // addFiltersToValues takes a slice of strings, a Filters, and a pointer to a
 // url.Values. If those strings are present in the Filters, it adds them to the
 // url.Values.
+//
+// For instance, the "List Charges" endpoint takes a "customer", but the "List
+// Customers" endpoint does not. This allows both of these:
+//
+//     // Charges
+//     addFiltersToValues([]string{"count", "offset", "customer"}...)
+//     // Customers
+//     addFiltersToValues([]string{"count", "offset"}...)
 func addFiltersToValues(keys []string, filters Filters, values *url.Values) {
 
 	for i := 0; i < len(keys); i++ {
